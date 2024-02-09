216db4e4904605c1042b7f37268e3533ee429605
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-02-09 17:15:01 +0530

    loading-bar
    - npm i react-top-loading-bar
    - Implementing loading bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import './App.css';
+import LoadingBar from 'react-top-loading-bar';
 import RequestEpisode from './components/Pages/RequestEpisode';
+import './App.css';
 
 function App() {
   const [value, setValue] = useState(1);
   const [episodeNumber, setEpisodeNumber] = useState(1);
   const [startPlayback, setStartPlayback] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const handleOnChange = (event) => {
     const newValue = parseInt(event.target.value);
@@ -25,6 +27,7 @@ function App() {
 
   return (
     <div className='container'>
+      <LoadingBar height={4} color='#f11946' progress={progress} />
       <h1 className='display-1 text-center my-2'>
         <img src="https://raw.githubusercontent.com/Avdhesh-Varshney/Chanakya/main/src/assets/Chanakya.png" alt="Chanakya-Image" style={{ width: '5rem' }} />
         चाणक्य नीति
@@ -38,7 +41,7 @@ function App() {
           <input type="number" id="inputNumber" className="form-control" value={value} onKeyDown={handleKeyPress} onChange={handleOnChange} />
         </div>
       </div>
-      {startPlayback && <RequestEpisode episodeNumber={episodeNumber} />}
+      {startPlayback && <RequestEpisode episodeNumber={episodeNumber} setProgress={setProgress} />}
     </div>
   );
 }
--- src/components/Pages/RequestEpisode.jsx
+++ src/components/Pages/RequestEpisode.jsx
@@ -17,29 +17,34 @@ const segregate = (url) => {
 };
 
 const RequestEpisode = (props) => {
-  const { episodeNumber } = props;
+  const { episodeNumber, setProgress } = props;
   const [content, setContent] = useState('');
   const [url, setUrl] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
+      setProgress(5);
       let episodeName = '';
       if (episodeNumber < 10) {
         episodeName = `Ep 0${episodeNumber}`;
       } else {
         episodeName = `Ep ${episodeNumber}`;
       }
+      setProgress(10);
       try {
         const response = await fetch('https://api.github.com/repos/hack-boi/Chanakya/contents');
+        setProgress(30);
         const data = await response.json();
         const audioFiles = data
           .filter(file => file.name.endsWith('.mp3') || file.name.endsWith('.wav') || file.name.endsWith('.m4a') || file.name.endsWith('.aac'))
           .map(file => file.download_url);
 
+        setProgress(55);
         const URL = audioFiles.find(url => url.includes(episodeName));
         if (!URL) {
           throw new Error(`${episodeName} not found`);
         }
+        setProgress(80);
         let trimData = segregate(URL);
         setContent(trimData.content);
         setUrl(trimData.url);
@@ -47,6 +52,7 @@ const RequestEpisode = (props) => {
         console.error('Error fetching episode:', error);
         setContent('Error fetching episode');
       }
+      setProgress(100);
     };
 
     fetchData();
