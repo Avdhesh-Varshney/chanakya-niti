c1176bbc1b7b6b3aa2872a7b57a7aaad98bd3df4
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-02-09 16:48:52 +0530

    user-chosen-functionality
    Implementing user chosen functionality of episode number of Chanakya Niti.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
-import './App.css'
-import CardsBox from './components/Cards/CardsBox';
+import React, { useState } from 'react';
+import './App.css';
+import RequestEpisode from './components/Pages/RequestEpisode';
 
 function App() {
+  const [value, setValue] = useState(1);
+  const [episodeNumber, setEpisodeNumber] = useState(1);
+  const [startPlayback, setStartPlayback] = useState(false);
+
+  const handleOnChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    if (newValue > 0) {
+      setValue(newValue);
+    } else {
+      setValue(1);
+    }
+  };
+  
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      setEpisodeNumber(value);
+      setStartPlayback(true);
+    }
+  };
+
   return (
     <div className='container'>
-      <h1 className='display-1 text-center my-2'>Chanakya</h1>
-      <CardsBox />
+      <h1 className='display-1 text-center my-2'>
+        <img src="https://raw.githubusercontent.com/Avdhesh-Varshney/Chanakya/main/src/assets/Chanakya.png" alt="Chanakya-Image" style={{ width: '5rem' }} />
+        चाणक्य नीति
+      </h1>
+      <div className="row g-3 text-center align-items-center justify-content-center mb-5">
+        <h5>Enter episode number and press enter to start the player.</h5>
+        <div className="col-auto">
+          <label htmlFor="inputNumber" className="col-form-label">Episode Number</label>
+        </div>
+        <div className="col-auto">
+          <input type="number" id="inputNumber" className="form-control" value={value} onKeyDown={handleKeyPress} onChange={handleOnChange} />
+        </div>
+      </div>
+      {startPlayback && <RequestEpisode episodeNumber={episodeNumber} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
