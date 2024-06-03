3ccc1065c0136570e76d5b9f721216d646cc9f7e
Atanu789 <atanurandhawa@gmail.com>
2024-06-03 15:23:24 +0530

    animated-background and player card css

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import './Card.css'; 
 
 const Card = (props) => {
   const { title, content, url } = props;
 
   return (
-    <div className="card bg-white text-bg-light" style={{ width: '18rem' }}>
+    <div className="card custom-card">
       <img src="https://raw.githubusercontent.com/Avdhesh-Varshney/Chanakya/main/src/assets/Chanakya.webp" className="card-img" alt="Chanakya-Image" />
-      <div className="card-img p-3">
+      <div className="card-img-overlay p-3">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{content}</p>
         <AudioPlayer
@@ -16,7 +17,7 @@ const Card = (props) => {
           customAdditionalControls={[]}
           customVolumeControls={[]}
           showDownloadProgress={[]}
-          className='bg-transparent'
+          className='audio-player'
         />
       </div>
     </div>
