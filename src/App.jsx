6d0defaee760d194518a0f4816b410343b0a7935
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-06-15 09:13:31 +0530

    indent-fix
    - standardize the indentation of the whole code.
    - adding .vscode settings for 2-space indentation for editor.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import LoadingBar from 'react-top-loading-bar';
-import RequestEpisode from './components/Pages/RequestEpisode';
-import Alert from './components/Alert/Alert';
-import './App.css';
-import QuoteSection from './components/Quotes/QuotesSection';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import { options } from './assets/options.js';
+import React, { useState, useEffect } from "react";
+import LoadingBar from "react-top-loading-bar";
+import RequestEpisode from "./components/Pages/RequestEpisode";
+import Alert from "./components/Alert/Alert";
+import "./App.css";
+import QuoteSection from "./components/Quotes/QuotesSection";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { options } from "./assets/options.js";
 
 
 function App() {
@@ -21,12 +21,12 @@ function App() {
     if (!isNaN(newValue)) {
       setValue(newValue);
     } else {
-      setValue('');
+      setValue("");
     }
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       setEpisodeNumber(value);
       setStartPlayback(true);
     }
@@ -45,22 +45,21 @@ function App() {
     setAlert({
       msg: message
     })
-     setTimeout(() => {
-       setAlert(null);
-     }, 3500);
+    setTimeout(() => {
+      setAlert(null);
+    }, 3500);
   }
 
   return (
-    <div className='container'>
-      <LoadingBar height={4} color='#f11946' progress={progress} />
+    <div className="container">
+      <LoadingBar height={4} color="#f11946" progress={progress} />
       {alert && <Alert alert={alert} />}
 
-      <h1 className='display-1 text-center my-2'>
-        <img src="https://raw.githubusercontent.com/Avdhesh-Varshney/Chanakya/main/src/assets/Chanakya-Logo.webp" alt="Chanakya-Image" style={{ width: '5rem' }} />
+      <h1 className="display-1 text-center my-2">
+        <img src="https://raw.githubusercontent.com/Avdhesh-Varshney/Chanakya/main/src/assets/Chanakya-Logo.webp" alt="Chanakya-Image" style={{ width: "5rem" }} />
         चाणक्य नीति
       </h1>
-      <QuoteSection/>
-
+      <QuoteSection />
 
       <div className="row g-3 text-center align-items-center justify-content-center mb-5">
         <div className="col-auto">
@@ -71,14 +70,13 @@ function App() {
           <Autocomplete
             disablePortal
             className="bg-white"
-
             options={options}
             sx={{ width: 300 }}
             renderInput={(params) => <TextField onSelect={handleOnChange} onChange={handleOnChange} {...params} label="Input Ep.1 to 806 (Finale)" />}
           />
-          
         </div>
-        <div className='col-auto'>
+
+        <div className="col-auto">
           <button
             onClick={handleKey}
             className="px-4 py-2 text-white bg-blue-500 hover:bg-blue-700 rounded"
@@ -96,7 +94,7 @@ function App() {
           <p>Explore the life and teachings of Chanakya, an ancient Indian philosopher, economist, and political strategist. Learn about his contributions to Indian philosophy and political science.</p>
         </div>
       </div>
-      
+
       {startPlayback && <RequestEpisode episodeNumber={episodeNumber} setEpisodeNumber={setEpisodeNumber} setProgress={setProgress} />}
     </div>
   );
