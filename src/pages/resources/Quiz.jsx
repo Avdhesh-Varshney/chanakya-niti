4c7e74cde97320e7d0f8292ebba9cd53c7fc4168
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-06-24 10:03:31 +0530

    quiz-section
    - Added quiz section related to chanakya's life and it's behaviour and current news.

---
+++ src/pages/resources/Quiz.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../../components/shared/Card';
+
+const Quiz = () => {
+  return (
+    <Card
+      imgSrc="/src/assets/image.webp"
+      title="Chanakya Niti"
+      description="Hello World"
+      path="/resources/quiz/chanakya"
+      btnName="Start Quiz"
+    />
+  )
+}
+
+export default Quiz;
