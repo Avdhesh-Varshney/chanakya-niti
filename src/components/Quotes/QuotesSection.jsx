7cb87e450cc8e0bfe181b78d7b579c5a086b7913
ROKUMATE <rohitkumawat0110@gmail.com>
2024-06-28 03:04:55 +0530

    Solved the issue for Exposed authInfo and made it in .env file

--- src/components/Quotes/QuotesSection.jsx
+++ src/components/Quotes/QuotesSection.jsx
@@ -3,45 +3,49 @@ import quotes from "/src/database/quotes.json";
 import "./QuoteSection.css";
 
 const QuoteSection = () => {
-  const [quote, setQuote] = useState("");
+    const [quote, setQuote] = useState("");
+    const AuthInfo = process.env.QUOTES_API_KEY;
 
-  useEffect(() => {
-    fetchDailyQuote();
-  }, []);
+    useEffect(() => {
+        fetchDailyQuote();
+    }, []);
 
-  const fetchDailyQuote = async () => {
-    try {
-      const response = await fetch("https://api.yourquoteapi.com/quotes?author=Chanakya", {
-        headers: {
-          "Authorization": `4fe303f4bamshb97ef5b1dd575e0p1ccc51jsnd9644d3c587b`
+    const fetchDailyQuote = async () => {
+        try {
+            const response = await fetch(
+                "https://api.yourquoteapi.com/quotes?author=Chanakya",
+                {
+                    headers: {
+                        Authorization: AuthInfo,
+                    },
+                }
+            );
+            const data = await response.json();
+            if (data && data.length > 0) {
+                setQuote(data[0].quote);
+            } else {
+                setFallbackQuote();
+            }
+        } catch (error) {
+            console.error("Error fetching the quote:", error);
+            setFallbackQuote();
         }
-      });
-      const data = await response.json();
-      if (data && data.length > 0) {
-        setQuote(data[0].quote);
-      } else {
-        setFallbackQuote();
-      }
-    } catch (error) {
-      console.error("Error fetching the quote:", error);
-      setFallbackQuote();
-    }
-  };
+    };
 
-  const setFallbackQuote = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    setQuote(quotes[randomIndex].quote);
-  };
+    const setFallbackQuote = () => {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[randomIndex].quote);
+    };
 
-  return (
-    <div className="d-flex align-items-center justify-content-center">
-      <div className="d-flex flex-column flex-md-row align-items-center">
-        <div className="text-center text-md-start">
-          <p className="quote-text">"{quote}"</p>
+    return (
+        <div className="d-flex align-items-center justify-content-center">
+            <div className="d-flex flex-column flex-md-row align-items-center">
+                <div className="text-center text-md-start">
+                    <p className="quote-text">"{quote}"</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default QuoteSection;
