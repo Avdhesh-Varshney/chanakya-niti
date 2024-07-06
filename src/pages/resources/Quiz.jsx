332b728d67b4f4894660b93d66b61e0b14b3c8f9
Shivam Verma <shivam.verma256@outlook.com>
2024-07-06 14:21:01 +0530

    resources img fix

--- src/pages/resources/Quiz.jsx
+++ src/pages/resources/Quiz.jsx
@@ -4,7 +4,7 @@ import Card from '../../components/shared/Card';
 const Quiz = () => {
   return (
     <Card
-      imgSrc="/src/assets/image.webp"
+      imgSrc="/image.webp"
       title="Chanakya Niti"
       description="Hello World"
       path="/resources/quiz/chanakya"
