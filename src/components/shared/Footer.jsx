eafabdbf1dafd501ebf16077ed3abd817a9f7eee
Shivam Verma <shivam.verma256@outlook.com>
2024-07-08 14:05:35 +0530

    Added Footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React from "react";
+import "../../css/Footer.css";
+import { Link, NavLink } from "react-router-dom";
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="mx-auto px-4 py-2">
-        <center><p>&copy; 2024 Chanakya Niti. All rights reserved.</p></center>
+const Footer = () => (
+  <footer className="page-footer font-small blue pt-4 mb-4">
+    <div className="container-fluid text-center text-md-left">
+      <div className="row">
+        <div className="col-md-6 mt-md-0 mt-3 flex flex-row">
+          <img
+            src="/logo.webp"
+            height="220"
+            alt="footer logo"
+            className="pt-4"
+          />
+          <h5 className="pt-5 m-0 mt-2 fw-bolder fs-1">चाणक्य नीति</h5>
+        </div>
+
+        <hr className="clearfix w-100 d-md-none pb-0" />
+
+        <div className="col-md-3 mb-md-0 mb-3 pt-4">
+          <h5 className="text-uppercase fw-bold">चाणक्य नीति</h5>
+          <ul className="list-unstyled">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contributor">Contributors</NavLink>
+            </li>
+            <li>
+              <NavLink to="/auth/SignIn">Sign-In</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-md-3 mb-md-0 mb-3 pt-3">
+          <h5 className="text-uppercase fw-bold">Resources</h5>
+          <ul className="list-unstyled">
+            <li>
+              <NavLink to="/resources/audio">Audio</NavLink>
+            </li>
+            <li>
+              <NavLink to="/resources/video">Video</NavLink>
+            </li>
+            <li>
+              <NavLink to="/resources/book">Books</NavLink>
+            </li>
+            <li>
+              <NavLink to="/resources/news">News</NavLink>
+            </li>
+            <li>
+              <NavLink to="/resources/quiz">Quiz</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </footer>
-  );
-};
+    </div>
+    <div className="footer-copyright text-center pt-2">
+      <center>
+        <p>&copy; 2024 Chanakya Niti. All rights reserved.</p>
+      </center>
+    </div>
+  </footer>
+);
 
 export default Footer;
