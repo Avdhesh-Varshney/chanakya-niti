b6d8d686219e161f155997228d90a48126d865c1
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-16 14:57:07 +0530

    nav-update

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,17 +1,33 @@
-import { ThemeContext } from '../../App';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {FaSun,FaMoon} from 'react-icons/fa6'
-const Navbar = () => {
-  let {theme,toggleTheme}=useContext(ThemeContext)
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-2 rounded">
-      <div className="container-fluid">
-        {/* Attach logo of website */}
-        <Link className="navbar-brand" to="/">चाणक्य नीति</Link>
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
+import { Context } from '../../context/Context';
 
-        <button
-          className="navbar-toggler"
+const Navbar = () => {
+  const { isDarkMode, toggleTheme } = useContext(Context);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) setIsSticky(true);
+    else setIsSticky(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar navbar-expand-lg ${isSticky ? 'mt-2 fixed-top' : ''} ${isDarkMode ? 'navbar-dark' : 'navbar-light'}`} style={{ backgroundColor: `${isDarkMode ? 'rgba(0, 0, 0, 0.4)' : 'rgba(223, 223, 176, 0.4)'}` }}>
+      <div className="container-fluid">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img src="logo.webp" alt="Chanakya Image" className="me-2" style={{ width: '30px', height: '30px' }} />
+          <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>चाणक्य नीति</span>
+        </Link>
+
+        <button className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -21,10 +37,9 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
 
             <li className="nav-item dropdown">
@@ -39,21 +54,17 @@ const Navbar = () => {
                 <li><Link className="dropdown-item" to="/resources/quiz">Quiz</Link></li>
               </ul>
             </li>
-            
+
             <li className="nav-item"><Link className="nav-link" to="/contributor">Contributors</Link></li>
-            
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/SignIn">
-                <button type='button' className='btn btn-outline-light'>Sign In</button>
-              </Link>
+            <li className="nav-item"><Link className="nav-link" to="/auth/login">Login</Link></li>
+
+            <li className='nav-item nav-link' onClick={toggleTheme} style={{ cursor: 'pointer' }}>
+              {isDarkMode ? (
+                <MdOutlineLightMode style={{ fontSize: '1.5rem', color: 'white' }} />
+              ) : (
+                <MdOutlineDarkMode style={{ fontSize: '1.5rem', color: 'black' }} />
+              )}
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/SignUp">
-                <button type='button' className='btn btn-light'>Sign Up</button>
-              </Link>
-            </li>
-            <li onClick={toggleTheme} className='mode'>
-              {theme == 'dark' ?<FaSun style={{color:"white"}}/>:<FaMoon style={{color:"white"}}/> }</li>
           </ul>
         </div>
       </div>
