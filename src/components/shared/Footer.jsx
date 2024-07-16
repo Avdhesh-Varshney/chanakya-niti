19e5f15dc5bbd63d5b58c3f08eb868c4358d4e9d
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-16 14:57:19 +0530

    footer-update

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,74 +1,57 @@
-import React from "react";
-import "../../css/Footer.css";
-import { Link, NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { Context } from "../../context/Context";
 import Visitors from "./Visitors";
+import { FaGithub, FaHome, FaInfoCircle, FaUser, FaSignInAlt, FaMusic, FaVideo, FaBook, FaNewspaper, FaQuestionCircle } from "react-icons/fa";
 
-const Footer = () => (
-  <footer className="page-footer font-small blue pt-4 mb-4">
-    <div className="container-fluid text-center text-md-left">
-      <div className="row">
-        <div className="col-md-6 mt-md-0 mt-3 flex flex-row">
-          <img
-            src="/logo.webp"
-            height="220"
-            alt="footer logo"
-            className="pt-4"
-          />
-          <div>
-          <h5 className="pt-5 m-0 mt-2 fw-bolder fs-1 mb-3">चाणक्य नीति</h5>
-          <a href="https://github.com/Avdhesh-Varshney/chanakya-niti" target="_blank" className="text-white bg-dark p-2 rounded text-decoration-none">Star Us ⭐</a>
-          <Visitors />
+const Footer = () => {
+  const { isDarkMode } = useContext(Context);
+
+  return (
+    <footer className="footer font-small pt-4" style={{ backgroundColor: `${isDarkMode ? 'rgba(0, 0, 0, 0.4)' : 'rgba(223, 223, 176, 0.4)'}` }}>
+      <div className="container pt-2">
+        <div className="row">
+          <div className="col-md-6 d-flex">
+            <figure className="figure">
+              <img src="logo.webp" height="200" alt="Chanakya Image" />
+              <figcaption className="figure-caption text-center">चाणक्य नीति</figcaption>
+            </figure>
+            <div className="my-auto">
+              <a href="https://github.com/Avdhesh-Varshney/chanakya-niti" target="_blank" rel="noopener noreferrer" className="text-white bg-dark p-2 rounded text-decoration-none d-inline-block">
+                <FaGithub className="me-2" /> Star Us ⭐
+              </a>
+              <Visitors />
+            </div>
+          </div>
+
+          <div className="col-md-3 m-auto">
+            <h3 className="text-uppercase fw-bold">चाणक्य नीति</h3>
+            <ul className="list-unstyled">
+              <li><NavLink to="/" className="text-decoration-none text-dark"><FaHome className="me-2" /> Home</NavLink></li>
+              <li><NavLink to="/about" className="text-decoration-none text-dark"><FaInfoCircle className="me-2" /> About</NavLink></li>
+              <li><NavLink to="/contributor" className="text-decoration-none text-dark"><FaUser className="me-2" /> Contributors</NavLink></li>
+              <li><NavLink to="/auth/login" className="text-decoration-none text-dark"><FaSignInAlt className="me-2" /> Login</NavLink></li>
+            </ul>
+          </div>
+
+          <div className="col-md-3 my-auto">
+            <h3 className="text-uppercase fw-bold">Resources</h3>
+            <ul className="list-unstyled">
+              <li><NavLink to="/resources/audio" className="text-decoration-none text-dark"><FaMusic className="me-2" /> Audio</NavLink></li>
+              <li><NavLink to="/resources/video" className="text-decoration-none text-dark"><FaVideo className="me-2" /> Video</NavLink></li>
+              <li><NavLink to="/resources/book" className="text-decoration-none text-dark"><FaBook className="me-2" /> Books</NavLink></li>
+              <li><NavLink to="/resources/news" className="text-decoration-none text-dark"><FaNewspaper className="me-2" /> News</NavLink></li>
+              <li><NavLink to="/resources/quiz" className="text-decoration-none text-dark"><FaQuestionCircle className="me-2" /> Quiz</NavLink></li>
+            </ul>
           </div>
         </div>
-
-        <hr className="clearfix w-100 d-md-none pb-0" />
-
-        <div className="col-md-3 mb-md-0 mb-3 pt-4">
-          <h5 className="text-uppercase fw-bold">चाणक्य नीति</h5>
-          <ul className="list-unstyled">
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contributor">Contributors</NavLink>
-            </li>
-            <li>
-              <NavLink to="/auth/SignIn">Sign-In</NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <div className="col-md-3 mb-md-0 mb-3 pt-3">
-          <h5 className="text-uppercase fw-bold">Resources</h5>
-          <ul className="list-unstyled">
-            <li>
-              <NavLink to="/resources/audio">Audio</NavLink>
-            </li>
-            <li>
-              <NavLink to="/resources/video">Video</NavLink>
-            </li>
-            <li>
-              <NavLink to="/resources/book">Books</NavLink>
-            </li>
-            <li>
-              <NavLink to="/resources/news">News</NavLink>
-            </li>
-            <li>
-              <NavLink to="/resources/quiz">Quiz</NavLink>
-            </li>
-          </ul>
-        </div>
       </div>
-    </div>
-    <div className="footer-copyright text-center pt-2">
-      <center>
+
+      <div className="text-center">
         <p>&copy; 2024 Chanakya Niti. All rights reserved.</p>
-      </center>
-    </div>
-  </footer>
-);
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
