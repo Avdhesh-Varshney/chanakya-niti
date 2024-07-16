e757f1c14f80d6f4fdbdadd0bd10eb81ce3755f7
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-16 14:55:42 +0530

    created-context-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
+import { Context } from "./context/Context";
 
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
@@ -22,66 +23,49 @@ import ChanakyaQuiz from "./pages/resources/quiz/ChanakyaQuiz";
 import Contributors from "./pages/contributor/Contributors";
 import ContributorDetail from "./pages/contributor/ContributorDetail";
 
-import SignIn from "./pages/auth/SignIn";
+import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
-import { createContext } from 'react';
 
-
-//theme context
-export const ThemeContext = createContext(null);
 function App() {
-  const [progress, setProgress] = useState(0);
-  const [theme, setTheme] = useState("light");
+  const { progress } = useContext(Context);
 
-  //Toggle theme
-  const toggleTheme = () => {
-    console.log('cliocking')
-document.body.classList.toggle('dark')
-    if(theme=='dark'){
-      setTheme('light')
-    }else{
-      setTheme('dark')
-    }
-  }
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
-
-    <div className='container d-flex flex-column min-vh-100'>
+    <div className='d-flex flex-column'>
       <Router>
         <Navbar />
         <LoadingBar height={3} color="#f11946" progress={progress} />
 
-        <main className="flex-grow-1 container mt-4">
+        <main className="flex-grow-1 mt-4">
           <Routes>
-            <Route exact path="/" element={<Home setProgress={setProgress} />} />
-            <Route exact path="/about" element={<About setProgress={setProgress} />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
 
             {/* Audio Resource Pages */}
             <Route exact path="/resources/audio" element={<Audio />} />
-            <Route exact path="/resources/audio/chanakya" element={<ChanakyaAudio setProgress={setProgress} />} />
+            <Route exact path="/resources/audio/chanakya" element={<ChanakyaAudio />} />
 
             {/* Video Resource Pages */}
             <Route exact path="/resources/video" element={<Video />} />
-            <Route exact path="/resources/video/chanakya" element={<ChanakyaVideo setProgress={setProgress} />} />
+            <Route exact path="/resources/video/chanakya" element={<ChanakyaVideo />} />
 
             {/* Book Resource Pages */}
             <Route exact path="/resources/book" element={<Book />} />
-            <Route exact path="/resources/book/chanakya" element={<ChanakyaBook setProgress={setProgress} />} />
+            <Route exact path="/resources/book/chanakya" element={<ChanakyaBook />} />
 
             {/* News Resource Pages */}
             <Route exact path="/resources/news" element={<News />} />
-            <Route exact path="/resources/news/chanakya" element={<ChanakyaNews setProgress={setProgress} />} />
+            <Route exact path="/resources/news/chanakya" element={<ChanakyaNews />} />
 
             {/* Quiz Resource Pages */}
             <Route exact path="/resources/quiz" element={<Quiz />} />
-            <Route exact path="/resources/quiz/chanakya" element={<ChanakyaQuiz setProgress={setProgress} />} />
+            <Route exact path="/resources/quiz/chanakya" element={<ChanakyaQuiz />} />
 
-            <Route exact path="/contributor" element={<Contributors setProgress={setProgress} />} >
+            <Route exact path="/contributor" element={<Contributors />} >
             </Route>
-            <Route path="/contributor/details" element={<ContributorDetail setProgress={setProgress}/>}> </Route>
+            <Route path="/contributor/details" element={<ContributorDetail />}> </Route>
 
             {/* Authentication Pages */}
-            <Route exact path="/auth/SignIn" element={<SignIn />} />
+            <Route exact path="/auth/login" element={<Login />} />
             <Route exact path="/auth/SignUp" element={<SignUp />} />
           </Routes>
         </main>
@@ -89,7 +73,6 @@ document.body.classList.toggle('dark')
         <Footer />
       </Router>
     </div>
-    </ThemeContext.Provider>
   );
 }
 
