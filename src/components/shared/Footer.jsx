f341121100bb650254ea46b72e3d4ee5248a9b11
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-10-19 21:42:10 +0530

    Fixes #298: Update footer layout for mobile view

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -13,22 +13,30 @@ const Footer = () => {
     <footer className="footer font-small pt-4" style={{ backgroundColor: `${isDarkMode ? 'rgba(0, 0, 0, 0.4)' : 'rgba(223, 223, 176, 0.4)'}` }}>
       <div className="container pt-2">
         <div className="row">
-          <div className="col-md-6 d-flex">
-            <figure className="figure">
-              <Tilt>
-                <img src="logo.webp" height="200" alt="Chanakya Image" />
-              </Tilt>
-              <figcaption className="figure-caption text-center" style={{ color: isDarkMode ? 'white' : 'black' }}>चाणक्य नीति</figcaption>
-            </figure>
-            <div className="my-auto star-btn">
+          <div className="col-md-6 d-lg-flex">
+      
+          <figure className="figure d-flex flex-column align-items-center">
+  <Tilt>
+    <img src="logo.webp" height="200" alt="Chanakya Image" />
+  </Tilt>
+  <figcaption
+    className="figure-caption text-center mt-2"
+    style={{ color: isDarkMode ? 'white' : 'black' }}
+  >
+    चाणक्य नीति
+  </figcaption>
+</figure>
+
+       
+            <div className="my-auto star-btn d-flex flex-column align-items-center">
               <a href="https://github.com/Avdhesh-Varshney/chanakya-niti" target="_blank" rel="noopener noreferrer" className="text-white bg-dark p-2 rounded text-decoration-none d-inline-block">
                 <FaGithub className="me-2" /> Star Us ⭐
               </a>
               <Visitors />
             </div>
           </div>
-
-          <div className="col-md-3 m-auto">
+          
+          <div className="col">
             <h3 className="text-uppercase fw-bold" style={{ color: isDarkMode ? 'white' : 'black' }}>चाणक्य नीति</h3>
             <ul className="list-unstyled">
               <li><NavLink to="/" className="text-decoration-none" style={{ color: isDarkMode ? 'white' : 'black' }}><FaHome className="me-2" /> Home</NavLink></li>
@@ -38,7 +46,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="col-md-3 my-auto">
+          <div className="col my-auto">
             <h3 className="text-uppercase fw-bold" style={{ color: isDarkMode ? 'white' : 'black' }}>Resources</h3>
             <ul className="list-unstyled">
               <li><NavLink to="/resources/chanakyagpt" className="text-decoration-none" style={{ color: isDarkMode ? 'white' : 'black' }}><FaRobot className="me-2" /> ChanakyaGpt</NavLink></li>
