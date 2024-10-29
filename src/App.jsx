6c25ff4749b1d7b6745e41856b930c824c9e50b9
Edasgh <edas25564@gmail.com>
2024-10-29 20:58:51 +0530

    "Progressbar added & back-to-top button modified"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoadingBar from "react-top-loading-bar";
+
 import { Context } from "./context/Context";
 
 import Navbar from "./components/shared/Navbar";
@@ -27,11 +27,12 @@ import "./App.css";
 import AnimatedCursor from "react-animated-cursor";
 
 import { FaAngleDoubleUp } from "react-icons/fa";
+import { ScrollToTop } from "react-simple-scroll-up";
 
 function App() {
-  const { progress, isDarkMode } = useContext(Context); // Assuming isDarkMode is provided in your context
-
+  const {isDarkMode } = useContext(Context); // Assuming isDarkMode is provided in your context
   const [showScroll, setShowScroll] = useState(false);
+
 
   // Function to handle scroll to top
   const scrollToTop = () => {
@@ -66,6 +67,8 @@ function App() {
     document.body.classList.toggle("dark", isDarkMode);
   }, [isDarkMode]);
 
+
+
   return (
     <div className={`d-flex flex-column ${isDarkMode ? "dark" : ""}`}>
       <AnimatedCursor
@@ -91,8 +94,6 @@ function App() {
       />
       <Router>
         <Navbar />
-        <LoadingBar height={3} color="#f11946" progress={progress} />
-
         <main className="container flex-grow-1 mt-4">
           <Routes>
             <Route exact path="/" element={<Home />} />
@@ -124,9 +125,9 @@ function App() {
 
         <Footer />
       </Router>
-      
+
       {/* Scroll to Top Button */}
-      <button
+      {/* <button
         className="scroll-to-top"
         onClick={scrollToTop}
         style={{
@@ -134,7 +135,7 @@ function App() {
           bottom: "50px",
           right: "30px",
           display: showScroll ? "flex" : "none",
-          backgroundColor: isDarkMode ? "#333333" : "#DFDFB0",
+          backgroundColor: isDarkMode ? "#6f9e6f" : "#DFDFB0",
           color: isDarkMode ? "#F5FBFA" : "#333",
           borderRadius: "50%",
           height: "50px",
@@ -149,8 +150,17 @@ function App() {
         }}
       >
         <FaAngleDoubleUp />
-      </button>
-
+      </button> */}
+      <ScrollToTop
+        className="scroll-to-top"
+        symbol={<FaAngleDoubleUp />}
+        size={90}
+        bgColor={isDarkMode ? "#2C2A2A" : "#dfdfb0"}
+        strokeWidth={6}
+        strokeFillColor={isDarkMode?"#fff":"rgb(0, 0, 0)"}
+        strokeEmptyColor="#505050"
+        symbolColor={isDarkMode ? "#F5FBFA" : "#333"}
+      />
     </div>
   );
 }
