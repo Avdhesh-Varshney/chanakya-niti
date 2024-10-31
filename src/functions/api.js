e89d1775f0c8f20692170a9051dfbb606e9917d7
Edasgh <edas25564@gmail.com>
2024-10-31 18:54:54 +0530

    "BACKEND URL imported"

--- src/functions/api.js
+++ src/functions/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/auth",
+  baseURL: `${VITE_BACKEND_URL}`,
 });
 
 export const googleAuth=(code)=>api.get(`/google?code=${code}`);
