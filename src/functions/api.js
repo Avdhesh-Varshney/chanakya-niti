4e604f31e9432a04e27df605c7fc548149a97f86
Edasgh <edas25564@gmail.com>
2024-11-01 17:30:05 +0530

    "Oauth donee"

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,9 +1,10 @@
 //file created by : Edasgh
 import axios from "axios";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 const api = axios.create({
-  baseURL: `${VITE_BACKEND_URL}`,
+  baseURL: `${BACKEND_URL}`,
 });
 
-export const googleAuth=(code)=>api.get(`/google?code=${code}`);
+export const googleAuth=(code)=>api.get(`/api/auth/google?code=${code}`);
 
