d0c7f1385f326d4f1d44e0befd41cadbcda9c832
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-04-05 13:23:13 +0000

    update: backend

--- backend/app.js
+++ backend/app.js
@@ -6,26 +6,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const URI = process.env.MONGODB_URI;
 
-// Middleware
-const allowedOrigins = ['https://chanakya-niti.vercel.app', 'http://localhost:5173'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
-// Database Connection
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
@@ -35,18 +21,13 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
 connectDB();
 
-// Available Routes
-import auth from './routes/auth.js';
-
-app.use('/api/auth', auth);
 app.get('/', (req, res) => {
   res.send('Hello from Chanakya Niti Backend!');
 });
 
-
-// Start Server
 app.listen(port, () => {
   console.log(`Chanakya Niti Backend listening on port http://localhost:${port}`);
 });
