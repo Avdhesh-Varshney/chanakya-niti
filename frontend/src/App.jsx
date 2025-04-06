9d203fd497e539819452371b4d4faa26a091d45b
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-04-06 04:57:10 +0000

    setup login/register btns

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,50 @@
 import { Routes, Route } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+
+import { lookInSession } from "./common/session";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import ChanakyaAudio from "./pages/ChanakyaAudio";
+
+export const UserContext = createContext({});
 
 function App() {
+
+  const [userAuth, setUserAuth] = useState({});
+
+  useEffect(() => {
+    let userInSession = lookInSession("user");
+    userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null });
+  }, []);
+
   return (
-    <div className="flex flex-col min-h-screen bg-[#FFFFFF06]">
+    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+      <div className="flex flex-col min-h-screen bg-[#FFFFFF06]">
 
-      <div className="navbar bg-[#dfdfb0]">
-        <div className="w-full max-w-7xl mx-auto">
-          <Navbar />
+        <div className="navbar bg-[#dfdfb0] z-50">
+          <div className="w-full max-w-7xl mx-auto flex">
+            <Navbar />
+          </div>
         </div>
-      </div>
 
-      <main className="flex-1 w-full max-w-7xl px-4 mx-auto">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resources/audio" element={<ChanakyaAudio />} />
-        </Routes>
-      </main>
+        <main className="flex-1 w-full max-w-7xl px-4 mx-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<h1>Login Page</h1>} />
+            <Route path="/register" element={<h1>Register Page</h1>} />
+          </Routes>
+        </main>
 
-      <div className="footer bg-[#dfdfb0] mt-8">
-        <div className="w-full max-w-7xl mx-auto">
-          <Footer />
+        <div className="footer bg-[#dfdfb0] mt-8">
+          <div className="w-full max-w-7xl mx-auto">
+            <Footer />
+          </div>
         </div>
+
       </div>
-    </div>
+    </UserContext.Provider>
   );
 }
 
